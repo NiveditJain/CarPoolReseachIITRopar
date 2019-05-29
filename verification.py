7068df26d037ed8e663a39d998816e7dddb30a19
NiveditJain <niveditjain@outlook.com>
2019-05-29 16:12:38 +0530

    dataset generation succes

---
+++ verification.py
@@ -0,0 +1,69 @@
+from docplex.mp.model import Model # for all binary integer programming part
+import numpy as np #for converting pandas containers to regular integers
+import pandas as pd #for data management
+import gc #for garbage collection
+# importing all files
+print('code loaded in memory succesfully')
+def ImportDataFiles(): #to import the datafiles
+	PeopleData=pd.read_csv('PersonOriginAndSource.csv')
+	DistanceMatrix=pd.read_csv('distanceMatrix.csv')
+	return [PeopleData,DistanceMatrix]
+
+# opening the files
+PeopleData,DistanceMatrix=ImportDataFiles()
+print('datafiles opened succesfully')
+# creating the model for car pool verification team
+SRNC=Model('SRNC')
+print('initialized model')
+# to select the model as needed
+
+def SelectPeopleInNetWork():
+	print('Input the number of people you want in network(<=2000)')
+	selectedPeople=int(input())
+	PeopleData=PeopleData[:selectedPeople]
+	print('Selected first '+ str(selectedPeople)+' people from a list of 2000 randomly generated people from a list of 100k+')
+	return(PeopleData,selectedPeople)
+
+#we are considering an limit of 0.0000001 for equality to to floating point errors
+# this limit is a valid number as my dataset is well know from before
+def checkEquality(num1,num2):
+	return num1-num2<=0.0000001 
+
+# to get information of person
+# person info returned in the format
+def getPersonInfo(id,selectedPeople):
+	if(id[0]<2000):
+		if(id[1]<2000):
+			return [PeopleData.loc[id[0],['lat1','long1']].tolist(),PeopleData.loc[id[1],['lat1','long1']].tolist()]
+		else:
+			return [PeopleData.loc[id[0],['lat1','long1']].tolist(),PeopleData.loc[id[1]-selectedPeople,['lat2','long2']].tolist()]
+	else:
+		if(id[1]<2000):
+			return [PeopleData.loc[id[0]-selectedPeople,['lat2','long2']].tolist(),PeopleData.loc[id[1],['lat1','long1']].tolist()]
+		else:
+			return [PeopleData.loc[id[0]-selectedPeople,['lat2','long2']].tolist(),PeopleData.loc[id[1]-selectedPeople,['lat2','long2']].tolist()]	
+
+
+# to get distance time details of a person id
+def getWeight(id,selectedPeople):
+	PersonInfo=getPersonInfo(id,selectedPeople)
+	return DistanceMatrix.loc[\
+		(checkEquality(DistanceMatrix['lat1'],PersonInfo[0][0])) &
+		(checkEquality(DistanceMatrix['long1'],PersonInfo[0][1])) &
+		(checkEquality(DistanceMatrix['lat2'],PersonInfo[1][0])) & 
+		(checkEquality(DistanceMatrix['long2'],PersonInfo[1][1]))\
+		].dis.tolist()[0]
+
+
+# testing code beneath
+
+# print(PeopleData)
+# y=PeopleData.loc[0,['lat1','long1']].tolist()
+# y=getPersonInfo([0,2000],2000)
+# print(y)
+# temp=DistanceMatrix.loc[(checkEquality(DistanceMatrix['lat1'],y[0][0])) & (checkEquality(DistanceMatrix['long1'],y[0][1])) & (checkEquality(DistanceMatrix['lat2'],y[1][0])) & (checkEquality(DistanceMatrix['long2'],y[1][1]))].dis.tolist()[0]
+# print(temp)
+# print(getWeight([0,2000],2000))
+# print(type(y),type(y[0][0]))
+# print(getWeight([0,2000],2000))
+# print(checkEquality(92,92.0000000001))
